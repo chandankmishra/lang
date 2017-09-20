81051b378c938e463effdaf4a1e374557f9cec0f
Chandan Mishra <chandan.iitr@gmail.com>
2017-09-19 18:02:50 -0700

    added push and append apis.

--- python/ds/llist.py
+++ python/ds/llist.py
@@ -22,28 +22,58 @@ class LinkedList(object):
         self.head = None
 
     def printList(self):
-        print("Printing the nodes:")
+        print("Printing the nodes: ", end='')
         temp = self.head
         while(temp):
-            print (temp.data)
+            print ("{0} ".format(temp.data), end='')
             temp = temp.next
+
+    # Function to insert a node in the begining
+    def push(self, new_data):
+        # temp = self.head
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    # Function to insert a node at the end.
+    def append(self, new_data):
+        new_node = Node(new_data)
+
+        if self.head is None:
+            # If the list is empty just set the new node as the new head
+            self.head = new_node
+        else:
+            # Iterate through the list and get the last node
+            temp = self.head
+            while (temp.next):
+                temp = temp.next
+            temp.next = new_node
+
+    # Function to insert a node after the given preiv_ndoe
+    def insertAfter(self, prev_node, new_data):
+        pass
 
 
 def main():
-    # Start with empty list
+    # Start with the empty list
     llist = LinkedList()
 
-    # Create ndoes
-    llist.head = Node(1)
-    second = Node(2)
-    third = Node(3)
-    # n = LinkedList(None)
+    # Insert 6.  So linked list becomes 6->None
+    llist.append(6)
 
-    # Link the nodes
-    llist.head.next = second
-    second.next = third
+    # Insert 7 at the beginning. So linked list becomes 7->6->None
+    llist.push(7)
 
-    # Print the list
+    # Insert 1 at the beginning. So linked list becomes 1->7->6->None
+    llist.push(1)
+
+    # Insert 4 at the end. So linked list becomes 1->7->6->4->None
+    llist.append(4)
+
+    # Insert 8, after 7. So linked list becomes 1 -> 7-> 8-> 6-> 4-> None
+    llist.insertAfter(llist.head.next, 8)
+
+    print ('Created linked list is:')
     llist.printList()
 
 
