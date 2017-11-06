0b28cf6bf9b34409f184b5bdaf70f0cb70f47e8d
Chandan Mishra <chandan.iitr@gmail.com>
2017-11-06 04:31:59 -0800

    Add files via upload

--- python/problems/isomorphic-strings.py
+++ python/problems/isomorphic-strings.py
@@ -23,26 +23,53 @@ def isIsomorphic(s, t):
     :type t: str
     :rtype: bool
     """
-    map1 = {}
-    map2 = {}
-    for i in range(0, len(s)):
-        if s[i] not in map1:
-            map1[s[i]] = t[i]
-        else:
-            print (s, t, i)
-            print (map1[s[i]], t[i])
-            if map1[s[i]] != t[i]:
-                return False
-        if t[i] not in map2:
-            map2[t[i]] = s[i]
-        else:
-            print (s, t, i)
-            print (map2[t[i]], s[i])
-            if map2[t[i]] != s[i]:
-                return False
+    return len(set(s)) == len(set(zip(s, t))) == len(set(t))
 
-    return True
+# def isIsomorphic(s, t):
+#     """
+#     :type s: str
+#     :type t: str
+#     :rtype: bool
+#     """
+#     d = dict()
+#     r = set()
+#     for a, b in zip(s, t):
+#         if a in d:
+#             if d[a] != b:
+#                 return False
+#         else:
+#             if b in r:
+#                 return False
+#             d[a] = b
+#             r.add(b)
+#     return True
+
+# def isIsomorphic(s, t):
+#     """
+#     :type s: str
+#     :type t: str
+#     :rtype: bool
+#     """
+#     map1 = {}
+#     map2 = {}
+#     for i in range(0, len(s)):
+#         if s[i] not in map1:
+#             map1[s[i]] = t[i]
+#         else:
+#             print (s, t, i)
+#             print (map1[s[i]], t[i])
+#             if map1[s[i]] != t[i]:
+#                 return False
+#         if t[i] not in map2:
+#             map2[t[i]] = s[i]
+#         else:
+#             print (s, t, i)
+#             print (map2[t[i]], s[i])
+#             if map2[t[i]] != s[i]:
+#                 return False
+
+#     return True
 
 
 # print(isIsomorphic("egg", "add"))
-print(isIsomorphic("eggsta", "addtkk"))
+print(isIsomorphic("eggsta", "addtkm"))
