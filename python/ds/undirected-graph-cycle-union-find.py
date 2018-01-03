874e77f67e5ff72784fb53d4c9225ecfb17edcea
Chandan Mishra <chandan.iitr@gmail.com>
2018-01-02 20:34:26 -0800

    Add files via upload

--- python/ds/undirected-graph-cycle-union-find.py
+++ python/ds/undirected-graph-cycle-union-find.py
@@ -1,34 +1,51 @@
-def isCycleUtilDfs(graph, v, visited, parent):
-    visited.add(v)
-    #print (v, ",", end='')
+    def find_parent(parent, i):
+        if i not in parent:
+            return i
+        if parent[i] == -1:
+            return i
+        if parent[i] != -1:
+            return find_parent(parent, parent[i])
 
-    for i in graph[v]:
-        if i not in visited:
-            if isCycleUtilDfs(graph, i, visited, v):
-                return True
-        elif parent != i:
-            return True
-    return False
+    # A utility function to do union of two subsets
 
+    def union(parent, x, y):
+        x_set = find_parent(parent, x)
+        y_set = find_parent(parent, y)
+        parent[x_set] = y_set
 
-def isCycleDfs(graph, v):
-    visited = set()
-    return isCycleUtilDfs(graph, v, visited, -1)
+    # The main function to check whether a given graph
+    # contains cycle or not
 
+    def isCyclicUnionFind(graph):
+        # Allocate memory for creating V subsets and
+        # Initialize all subsets as single element sets
+        parent = {}
+        for i in graph:
+            parent[i] = -1
 
-graph1 = {'A': ['B', 'C', 'E'],
-          'B': ['C', 'E'],
-          'C': ['A'],
-          'D': ['C'],
-          'E': ['F'],
-          'F': ['C']}
+        for i in graph:
+            for j in graph[i]:
+                x = find_parent(parent, i)
+                y = find_parent(parent, j)
+                print (x, y)
+                if x == y:
+                    return True
+                union(parent, x, y)
+        return False
 
-graph2 = {'A': ['B'],
-          'B': ['E'],
-          'C': ['D'],
-          'D': ['C'],
-          'E': ['F'],
-          'F': ['C']}
+    graph1 = {'A': ['B', 'C', 'E'],
+              'B': ['C', 'E'],
+              'C': ['A'],
+              'D': ['C'],
+              'E': ['F'],
+              'F': ['C']}
 
-print (isCycleDfs(graph1, 'A'))
-print (isCycleDfs(graph2, 'A'))
+    graph2 = {'A': ['B'],
+              'B': ['E'],
+              'C': ['D'],
+              #'D': ['C'],
+              'E': ['F'],
+              'F': ['C']}
+
+    print (isCyclicUnionFind(graph1))
+    print (isCyclicUnionFind(graph2))
