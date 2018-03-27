76ef672c496c29e3321c325fa3b729ef56a28a5d
Chandan Kumar Mishra <cmishra@juniper.net>
2018-03-27 11:20:23 -0700

    dir changes

---
+++ cpp/cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp
+//
+//  Created by Chandan Kumar Mishra on 3/27/18.
+//  Copyright © 2018 Juniper Networks Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
--- cpp/problems/number-of-root-to-leaf-paths.cpp
+++ cpp/problems/number-of-root-to-leaf-paths.cpp
@@ -1,26 +1,21 @@
-/*
-Please note that it's Function problem i.e.
-you need to write your solution in the form of Function(s) only.
-Driver Code to call/invoke your function would be added by GfG's Online Judge.*/
+#include <iostream>
 
-
-/*  Tree node
 struct Node
 {
     int data;
     Node* left, * right;
-}; */
-
+};
+#if 0
 /*You are required to complete below method */
 void pathCountsH(Node *root, int count, std::map <int, int> &mymap)
 {
     //Your code here
-    if (root == NULL) {
+    if (root == nullptr) {
         return;
     }
     
     count += 1;
-    if (root->left == NULL && root->right == NULL) {
+    if (root->left == nullptr && root->right == nullptr) {
         if (mymap.find(count) == mymap.end()) {
             std::pair<int,int> mpair(count,1);
             mymap.insert(mpair);
@@ -40,3 +35,5 @@ void pathCounts(Node *root)
     for (auto& x: mymap)
         std::cout << x.first << " " << x.second << " $";
 }
+
+#endif
