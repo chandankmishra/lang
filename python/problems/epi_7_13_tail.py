fbb40f69f353984ea22b2be030712a781a65361e
chandankmishra <chandan.iitr@gmail.com>
2018-04-17 15:52:27 -0700

    commit msg.

---
+++ python/problems/epi_7_13_tail.py
@@ -0,0 +1,21 @@
+"""
+Implement a UNIX TAIL command
+"""
+def tail(file_name, n):
+    f = open(file_name, 'rb')
+    f.seek(0, 2)
+    file_size = f.tell()
+    newline_cnt = 0
+    print ("file size: ", file_size, " # of line:", n)
+
+    last_n_lines = ""
+    for i in range(1,file_size):
+       f.seek(-i, 2)
+       c = f.read(1).decode('utf-8')
+       if c == '\n': newline_cnt += 1 
+       if (newline_cnt >= n): break
+       last_n_lines += c
+    f.close()
+    return last_n_lines[::-1]
+
+print (tail("7_13_tail.cpp", 10))
