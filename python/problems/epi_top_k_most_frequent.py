111097c4495ddfaa9d036809122638f4bbf410cf
chandankmishra <chandan.iitr@gmail.com>
2018-04-26 14:10:32 -0700

    commit msg.

--- python/problems/epi_top_k_most_frequent.py
+++ python/problems/epi_top_k_most_frequent.py
@@ -6,21 +6,22 @@ def top_k_most_frequent(arr, k):
     ahash = defaultdict(int)
     res = list()
 
-    for i in arr:
-        ahash[i] += 1
+    for num in arr:
+        ahash[num] += 1
 
-    for i in set(arr):
-        heapq.heappush(res, (ahash[i], i))
-        if len(res) > 2:
+    for num in set(arr):
+        heapq.heappush(res, (ahash[num], num))
+        if len(res) > k:
             heapq.heappop(res)
     print (res)
 
     result = list()
-    for i in range(len(res)):
+    while res:
         var = heapq.heappop(res)
         result.append(var[1])
-    return (result)
+    result.reverse()
+    return result
 
 
 arr = [3, 1, 1, 1, 1, 4, 4, 5, 2, 6, 1, 5, 5, 6, 6]
-print (top_k_most_frequent(arr, 2))
+print (top_k_most_frequent(arr, 3))
