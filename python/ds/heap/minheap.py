df798e6e244acefa66e42bf88c2cb58302af9e9b
chandankmishra <chandan.iitr@gmail.com>
2018-04-26 23:00:10 -0700

    commit msg.

--- python/ds/heap/minheap.py
+++ python/ds/heap/minheap.py
@@ -13,47 +13,52 @@ class MinHeap:
     def _parent(self, index): return self.items[self._parentIndex(index)]
     def _right(self, index): return self.items[self._rightIndex(index)]
     def _left(self, index): return self.items[self._leftIndex(index)]
+    def _hasParent(self, index): return self._parentIndex(index) >= 0
+    def _hasRight(self, index): return self._rightIndex(index) < self.size
+    def _hasLeft(self, index): return self._leftIndex(index) < self.size
+    def _isEmpty(self): return True if self.size == 0 else False
+    def _isFull(self): return True if self.size and self.size == self.capacity else False
 
     def _heapifyUp(self):
         index = self.size - 1
-        while (index != 0 and self._parent(index) > self.items[index]): #compare the values
+        while (self._hasParent(index)  and self._parent(index) > self.items[index]): #compare the values
             self._swap(self._parentIndex(index), index) # swap the value @index
             index = self._parentIndex(index)
 
     def _heapifyDown(self):
         index = 0
-        while self._leftIndex(index) < self.size:
+        while self._hasLeft(index):
             # get the child index with the smaller value
             smallidx = self._leftIndex(index)
             if self._left(index) < self._right(index): smallidx = self._rightIndex(index)
-
             if self.items[index] < self.items[smallidx]:  break
-            else: self._swap(index, smallidx)
+            self._swap(index, smallidx)
             index = smallidx
 
     # operations which needed by the priority queue
     def insertKey(self, item):
         '''IMP: Add the key by value not index '''
-        if self.size == self.capacity:
-            return
-        print ("added ", item)
+        if self._isFull(): raise KeyError("Heap is already full.")
+
         self.items[self.size] = item
         self.size += 1
         self._heapifyUp()
+        print ("added ", item)
 
     def popmin(self):
         '''IMP Removes and return the minimum value '''
+        if _isEmpty(): raise KeyError("Heap is already empty.")
+
         tmp = self.items[0]
         self.items[0] = self.items[self.size - 1]
-        #self.items[self.size - 1] = 0
         self.size -= 1
         self._heapifyDown()
         return tmp
 
     def decreaseKey(self, index, val):
-        '''IMP Decreate the key @index by value (val) '''
+        '''IMP Decrease the key @index by value (val) '''
         self.items[index] = val
-        while index != 0 and self.items[self._parentIndex(index)] > self.items[index]:
+        while index != 0 and self._parent(index) > self.items[index]:
             self._swap(index, self._parentIndex(index))
             index = self._parentIndex(index)
 
