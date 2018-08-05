ed902d4d6bc377d963de646e0e43d549f7a9b249
chandankmishra <chandan.iitr@gmail.com>
2018-08-05 01:40:47 -0700

    commit msg.

--- python/concurrency/q_publisher_subscriber/meesage_broadcaster.py
+++ python/concurrency/q_publisher_subscriber/meesage_broadcaster.py
@@ -1,44 +1,53 @@
 # implement a message broadcaster (publisher subscriber)
 # fix the threading/locking part!
 
+import threading
+
 class Publisher:
     def __init__(self):
         self.observers = []
+        self.lock = threading.Lock()
 
     def register(self, observer):
+        self.lock.acquire()
         if not observer in self.observers:
             self.observers.append(observer)
+        self.lock.release()
 
     def unregister(self, observer):
+        self.lock.acquire()
         if observer in self.observers:
             self.observers.remove(observer)
+        self.lock.release()
 
-    def notify_observers(self, update, msg):  # update_observers
+    def notify_subscribers(self, update, msg):  # update_observers
+        self.lock.acquire()
         for observer in self.observers:
             observer.update(update, msg)
+        self.lock.release()
 
-
-class Observer:
+class Subscriber:
     def update(self, *args, **kwargs):
         pass
 
-
-class Subscriber1(Observer):
+class Subscriber1(Subscriber):
     def update(self, update, msg):
-        print ("American stock market received: {0} {1}".format(update, msg))
+        print ("subscriber1 received: {0} {1}".format(update, msg))
 
-
-class Subscriber2(Observer):
+class Subscriber2(Subscriber):
     def update(self, update, msg):
-        print ("European stock market received: {0} {1}".format(update, msg))
+        print ("subscriber2 received: {0} {1}".format(update, msg))
 
+publisher = Publisher()
 
-company = Publisher()
+subs = []
+subs.append(threading.Thread(target=publisher.register, args=(Subscriber1(), )))
+subs.append(threading.Thread(target=publisher.register, args=(Subscriber2(), )))
+for sub in subs:
+    sub.start()
 
-american_observer = Subscriber1()
-european_observer = Subscriber2()
+for i in range(10):
+    publisher.notify_subscribers('update:', msg='msg-' + str(i))
 
-company.register(american_observer)
-company.register(european_observer)
-
-company.notify_observers('important update:', msg='CEO quits')
+for sub in subs:
+    sub.join()
