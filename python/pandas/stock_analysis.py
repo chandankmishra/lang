74b4550b56eecac60cde340eb114dbedcd82b29d
chandankmishra <chandan.iitr@gmail.com>
2018-08-06 18:58:01 -0700

    commit msg.

--- python/pandas/stock_analysis.py
+++ python/pandas/stock_analysis.py
@@ -1,30 +1,99 @@
+#
 # script to get max stock value and current value of stock
+#
+
 import pandas_datareader.data as web
 import datetime
 import pandas as pd
 import MySQLdb
 import pdb
 
-end = datetime.date.today()
-start = end - datetime.timedelta(days=100)
-# stocks = ['TWTR']
-stocks = ['TWTR', 'NFLX', 'FB', 'TSLA', 'DBX', 'AMZN', 'GOOG', 'ZS', 'AMD', 'SNAP', 'MSFT', 'ADBE', 'BABA', 'PANW', 'ORCL', 'NVDA', 'VMW', 'ANET', 'YELP', 'INTU']
-res = {}
-result = []
+STOCKS = ['TWTR', 'NFLX', 'FB', 'TSLA', 'DBX', 'AMZN', 'GOOG', 'ZS', 'AMD', 'AAPL', 'SNAP', 'MSFT', 'ADBE', 'BABA', 'PANW', 'ORCL', 'NVDA', 'VMW', 'ANET', 'YELP', 'INTU']
 
-db = MySQLdb.connect(host="localhost",
-                     user="root",
-                     passwd="root",
-                     db="tmp")
-cursor = db.cursor()
+DB = None
+def connect_mysql_db():
+    global DB
+    DB = MySQLdb.connect(host="localhost",
+                         user="root",
+                         passwd="root",
+                         db="tmp")
+    return DB.cursor()
 
-for stock in stocks:
-  select_str = f"select date, {stock} from stock_price where {stock} = (select max({stock}) from stock_price);"
-  try:
-    cursor.execute(select_str)
-  except:
-    print ("ERROR", nsert_str)
-  row = cursor.fetchall()
-  print (stock, row)
-db.commit()
-db.close()
+def close_mysql_db():
+    DB.commit()
+    DB.close()
+
+def get_max_stock_price(results, cursor):
+    """
+    Get the max stock price in the current year
+    @todo: remove hardcoding for 2018.
+    """
+    for stock in STOCKS:
+      select_str = f"select date, {stock} from stock_price where {stock} = (select max({stock}) from stock_price where date >= '2018-01-01 00:00:00');"
+      try:
+        cursor.execute(select_str)
+      except:
+        print ("ERROR", nsert_str)
+      row = cursor.fetchall()
+      date, price = row[0][0], row[0][1]
+      results.append([stock, str(date), price])
+
+def get_current_price(results, cursor):
+    """
+    Get the current date stock price
+    @todo: find the last entry with valid data
+    """
+    select_str = f"select *  from stock_price order by date desc limit 1;"
+    try:
+        cursor.execute(select_str)
+    except:
+        print ("ERROR", nsert_str)
+    current_price = cursor.fetchall()
+    for i, price in enumerate(current_price[0][1:]):
+        if i == len(results): break
+        results[i].append(price)
+        increase = 0.0
+        if price > 0.0:
+            increase = "{0}".format(round(((results[i][2] - price) * 100) / price, 2))
+        results[i].insert(0, float(increase))
+
+def get_year_start_price(results, cursor):
+    """
+    Get the price at year start
+    @todo: find the first valid date in the year
+    """
+    select_str = f"select * from stock_price where date = '2018-01-02 00:00:00';"
+    try:
+        cursor.execute(select_str)
+    except:
+        print ("ERROR", nsert_str)
+    start_price = cursor.fetchall()
+    for i, price in enumerate(start_price[0][1:]):
+        if i == len(results): break
+        results[i].append(price)
+
+def display_result(results):
+    """
+    Display the result of analysis in tabular format
+    """
+    results = sorted(results, key=lambda x:-x[0])
+    print ('% increase\tcompany\tdate\t\tyear-start\tcurrent\thighest')
+    for r in results:
+        print ('{0}%\t\t{1}\t{2}\t{3}\t\t{4}\t{5}'.format(r[0], r[1], r[2], r[5], r[4], r[3]))
+
+def main():
+    """
+    main driver program
+    """
+    results = []
+    cursor = connect_mysql_db()
+    get_max_stock_price(results, cursor)
+    get_current_price(results, cursor)
+    get_year_start_price(results, cursor)
+    display_result(results)
+    close_mysql_db()
+
+if __name__ == "__main__":
+    main()
+
+#end of file
--- python/pandas/stock_data_insert.py
+++ python/pandas/stock_data_insert.py
@@ -1,4 +1,7 @@
-# Script to import data from Robinhood and Yahoo and insert into a database
+#
+# Script to import data from Robinhood and Yahoo and
+# insert into a mysql database
+#
 
 import pandas_datareader.data as web
 import datetime
@@ -6,67 +9,97 @@ import pandas as pd
 import MySQLdb
 import pdb
 
-end = datetime.date.today()
-start = end - datetime.timedelta(days=100)
-stocks = ['TWTR', 'NFLX', 'FB', 'TSLA', 'DBX', 'AMZN', 'GOOG', 'ZS', 'AMD', 'SNAP', 'MSFT', 'ADBE', 'BABA', 'PANW', 'ORCL', 'NVDA', 'VMW', 'ANET', 'YELP', 'INTU']
-res = {}
-result = []
+STOCKS = ['TWTR', 'NFLX', 'FB', 'TSLA', 'DBX', 'AMZN', 'GOOG', 'ZS', 'AMD', 'AAPL', 'SNAP', 'MSFT', 'ADBE', 'BABA', 'PANW', 'ORCL', 'NVDA', 'VMW', 'ANET', 'YELP', 'INTU']
 
-db = MySQLdb.connect(host="localhost",
-                     user="root",
-                     passwd="root",
-                     db="tmp")
-cursor = db.cursor()
+DB = None
+def connect_mysql_db():
+    global DB
+    DB = MySQLdb.connect(host="localhost",
+                         user="root",
+                         passwd="root",
+                         db="tmp")
+    return DB.cursor()
 
-# Fetch data from Robinhood
-for stock in stocks:
-  d = web.DataReader(stock, 'robinhood', '2015-02-01', '2015-03-01')
-  dstr = str(d).split('\n')
-  dstr = dstr[2:-2]
-  for i in range(len(dstr)):
-    tlst = dstr[i].split()
-    if i == 0:
-      company = tlst.pop(0)
+def close_mysql_db():
+    DB.commit()
+    DB.close()
 
-    date_str = tlst[0]
-    price = tlst[1]
-    if date_str == '...':
-      continue
-    dd = datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
-    insert_str = "UPDATE stock_price SET {} = {} where date='{}';".format(company, price, dd)
-    print (insert_str)
+def insert_rows(cursor):
+    select_query = f"select date from stock_price order by date desc limit 1;"
     try:
-      cursor.execute(insert_str)
+        cursor.execute(select_query)
     except:
-      print ("ERROR", nsert_str)
+        print ("ERROR", select_query)
+    last_date = cursor.fetchall()
+    start = last_date[0][0]
+    end = datetime.date.today()
+    while start <= end:
+        start += datetime.timedelta(days=1)
+        insert_query = f"insert into stock_price values('{start}',0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0);"
+        print (insert_query)
+        try:
+            cursor.execute(insert_query)
+        except:
+            print ("ERROR", insert_query)
 
-  # Fetch data from Yahoo
-  for stock in stocks:
-    try:
-      d = web.DataReader(stock, 'yahoo', '2016-05-01', '2016-05-31')
-    except:
-      continue
-    dstr = str(d).split('\n')
-    dstr = dstr[2:-2]
-    company = stock
-    for i in range(len(dstr)):
-      tlst = dstr[i].split()
-      date_str = tlst[0]
-      price = tlst[5]
-      try:
-        if float(price) > 2000 or float(price) == 0.0:
+def update_price_using_robinhood(cursor):
+    # Fetch data from Robinhood
+    end = datetime.date.today()
+    start = end - datetime.timedelta(days=30)
+    for stock in STOCKS:
+      #d = web.DataReader(stock, 'robinhood', start, end)
+      d = web.DataReader(stock, 'robinhood', '2018-08-06', '2018-08-06')
+      dstr = str(d).split('\n')
+      dstr = dstr[2:-2]
+      for i in range(len(dstr)):
+        tlst = dstr[i].split()
+        if i == 0:
+          company = tlst.pop(0)
+
+        date_str = tlst[0]
+        price = tlst[1]
+        if date_str == '...':
           continue
-      except:
-        continue
-      if date_str == '...':
-        continue
-      dd = datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
-      insert_str = "UPDATE stock_price SET {} = {} where date='{}';".format(company, price, dd, company)
-      print (insert_str)
-      try:
-        cursor.execute(insert_str)
-      except:
-        print ("ERROR", nsert_str)
+        dd = datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
+        update_query = "UPDATE stock_price SET {} = {} where date='{}';".format(company, price, dd)
+        print (update_query)
+        try:
+          cursor.execute(update_query)
+        except:
+          print ("ERROR", update_query)
 
-db.commit()
-db.close()
+def update_price_using_yahoo(cursor):
+    # Fetch data from Yahoo
+    for stock in STOCKS:
+      try:
+        #d = web.DataReader(stock, 'yahoo', start, end)
+        d = web.DataReader(stock, 'yahoo', '2018-08-01', '2018-08-06')
+      except:
+        continue
+      dstr = str(d).split('\n')
+      dstr = dstr[2:-2]
+      for row in dstr:
+        price = row.split()[4]
+        date_str = row.split()[0]
+        try:
+          if float(price) > 2000 or float(price) == 0.0:
+            continue
+        except:
+          continue
+        if date_str == '...':
+          continue
+        dd = datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
+        update_query = "UPDATE stock_price SET {} = {} where date='{}';".format(stock, price, dd, stock)
+        print (update_query)
+        try:
+          cursor.execute(update_query)
+        except:
+          print ("ERROR", update_query)
+
+cursor = connect_mysql_db()
+#update_price_using_robinhood(cursor)
+#update_price_using_yahoo(cursor)
+insert_rows(cursor)
+close_mysql_db()
+
+#end of file
