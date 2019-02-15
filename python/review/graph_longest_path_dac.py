a0da884a5bdf4882f44a6efa8c101bac78bd7833
chandankmishra <chandan.iitr@gmail.com>
2019-02-14 21:49:00 -0800

    commit msg.

--- python/review/graph_longest_path_dac.py
+++ python/review/graph_longest_path_dac.py
@@ -1,6 +1,6 @@
-#https://www.geeksforgeeks.org/shortest-path-for-directed-acyclic-graphs/
+#https://www.geeksforgeeks.org/longest-path-for-directed-acyclic-graphs/
 
-# Python program to find single source shortest paths 
+# Python program to find single source longest paths 
 # for Directed Acyclic Graphs Complexity :OV(V+E) 
 from collections import defaultdict 
   
@@ -9,12 +9,16 @@ from collections import defaultdict
 # the vertex to which edge connects. It also contains 
 # weight of the edge 
 class Graph: 
-    def __init__(self,vertices): 
-        self.V = vertices # No. of vertices 
-        self.graph = defaultdict(list) 
+    def __init__ (self): 
+        self.graph = {}
   
     def addEdge(self, u, v, w): 
-        self.graph[u].append((v, w)) 
+        if u not in self.graph:
+            self.graph[u] = set()
+        if v not in self.graph:
+            self.graph[v] = set()
+            
+        self.graph[u].add((v, w)) 
   
     def topologicalSortUtil(self,v,seen,stack): 
         seen.add(v)
@@ -28,21 +32,17 @@ class Graph:
         # Push current vertex to stack which stores topological sort 
         stack.append(v) 
   
-    def shortestPath(self, s): 
-        # Mark all the vertices as not seen 
+    def longestPath(self, source): 
         seen = set()
         stack =[] 
   
-        # Call the recursive helper function to store Topological 
-        # Sort starting from source vertice 
-        for i in range(self.V): 
-            if i not in seen:
-                self.topologicalSortUtil(s,seen,stack) 
+        # get topological sorted path from source to all vertexes 
+        self.topologicalSortUtil(source, seen, stack) 
   
         # Initialize distances to all vertices as infinite and 
         # distance to source as 0 
-        dist = [float("-inf")] * (self.V) 
-        dist[s] = 0
+        dist = [float("-inf")] * len(self.graph) 
+        dist[source] = 0
   
         # Process vertices in topological order 
         while stack: 
@@ -54,12 +54,12 @@ class Graph:
                 if dist[node] < dist[i] + weight: 
                     dist[node] = dist[i] + weight 
   
-        # Print the calculated shortest distances 
-        for i in range(self.V): 
+        # Print the calculated longest distances 
+        for i in self.graph: 
             print ("i=", i, "dist", dist[i]) if dist[i] != float("Inf") else  print("i=",i, "dist", "inf")
   
   
-g = Graph(6) 
+g = Graph() 
 g.addEdge(0, 1, 5) 
 g.addEdge(0, 2, 3) 
 g.addEdge(1, 3, 6) 
@@ -70,6 +70,6 @@ g.addEdge(2, 3, 7)
 g.addEdge(3, 4, -1) 
 g.addEdge(4, 5, -2) 
   
-s = 1 #source
-print ("Following are shortest distances from source %d " % s) 
-g.shortestPath(s) 
+source = 1 #source
+print ("Following are longest distances from source %d " % source) 
+g.longestPath(source) 
